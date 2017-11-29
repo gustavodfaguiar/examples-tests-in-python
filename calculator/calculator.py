caa3fb29fe63522cf5fcc73ce15520f08e9fc7b3
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-29 14:44:49 -0200

    Change method sum to static method

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,7 @@
 class Calculator:
-    def sum(self, value_one, value_two):
+    @staticmethod
+    def sum(value_one, value_two):
         if not all( type(value) is int for value in (value_one, value_two) ):
             raise Exception("Two integer values are expected")
-        
+
         return value_one + value_two
